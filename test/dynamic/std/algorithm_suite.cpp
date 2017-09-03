3d79f0d0dd895a653c4ebf855c583b98f417e015
Bjorn Reese <breese@users.sourceforge.net>
2017-09-03 18:29:11 +0200

    Added test suite for std::move algorithm

--- test/dynamic/std/algorithm_suite.cpp
+++ test/dynamic/std/algorithm_suite.cpp
@@ -2986,6 +2986,144 @@ void run()
 } // namespace mismatch_suite
 
 //-----------------------------------------------------------------------------
+// std::move
+//-----------------------------------------------------------------------------
+
+namespace move_suite
+{
+
+void move_null_to_array()
+{
+    variable data;
+    variable result = array::repeat(0, null);
+    std::move(data.begin(), data.end(), result.begin());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 0);
+}
+
+void move_boolean_to_array()
+{
+    variable data(true);
+    variable result = array::repeat(1, null);
+    std::move(data.begin(), data.end(), result.begin());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == true);
+}
+
+void move_integer_to_array()
+{
+    variable data(2);
+    variable result = array::repeat(1, null);
+    std::move(data.begin(), data.end(), result.begin());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 2);
+}
+
+void move_number_to_array()
+{
+    variable data(3.0);
+    variable result = array::repeat(1, null);
+    std::move(data.begin(), data.end(), result.begin());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 3.0);
+}
+
+void move_string_to_array()
+{
+    variable data("alpha");
+    variable result = array::repeat(1, null);
+    std::move(data.begin(), data.end(), result.begin());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == "alpha");
+}
+
+void move_array_to_array()
+{
+    variable data = { true, 2, 3.0, "alpha" };
+    variable result = array::repeat(data.size(), null);
+    std::move(data.begin(), data.end(), result.begin());
+
+    variable expect = array::make({ true, 2, 3.0, "alpha" });
+    TRIAL_PROTOCOL_TEST_ALL_WITH(result.begin(), result.end(),
+                                 expect.begin(), expect.end(),
+                                 std::equal_to<variable>());
+}
+
+void move_null_to_vector()
+{
+    variable data;
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 0);
+}
+
+void move_boolean_to_vector()
+{
+    variable data(true);
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == true);
+}
+
+void move_integer_to_vector()
+{
+    variable data(2);
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 2);
+}
+
+void move_number_to_vector()
+{
+    variable data(3.0);
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 3.0);
+}
+
+void move_string_to_vector()
+{
+    variable data("alpha");
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == "alpha");
+}
+
+void move_array_to_vector()
+{
+    variable data = { true, 2, 3.0, "alpha" };
+    std::vector<variable> result;
+    std::move(data.begin(), data.end(), std::back_inserter(result));
+
+    variable expect = array::make({ true, 2, 3.0, "alpha" });
+    TRIAL_PROTOCOL_TEST_ALL_WITH(result.begin(), result.end(),
+                                 expect.begin(), expect.end(),
+                                 std::equal_to<variable>());
+}
+
+void run()
+{
+    move_null_to_array();
+    move_boolean_to_array();
+    move_integer_to_array();
+    move_number_to_array();
+    move_string_to_array();
+    move_array_to_array();
+
+    move_null_to_vector();
+    move_boolean_to_vector();
+    move_integer_to_vector();
+    move_number_to_vector();
+    move_string_to_vector();
+    move_array_to_vector();
+}
+
+} // namespace move_suite
+
+//-----------------------------------------------------------------------------
 // std::none_of
 //-----------------------------------------------------------------------------
 
@@ -4328,6 +4466,7 @@ int main()
     lower_bound_suite::run();
     max_element_suite::run();
     mismatch_suite::run();
+    move_suite::run();
     none_of_suite::run();
     partition_point_suite::run();
     remove_suite::run();
