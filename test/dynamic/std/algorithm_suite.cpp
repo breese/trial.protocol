8b71a973f11d0447b3ee45bdea1798843dfda3b3
Bjorn Reese <breese@users.sourceforge.net>
2017-10-12 18:59:14 +0200

    Added test suite for std::transform

--- test/dynamic/std/algorithm_suite.cpp
+++ test/dynamic/std/algorithm_suite.cpp
@@ -7234,6 +7234,112 @@ void run()
 } // namespace search_suite
 
 //-----------------------------------------------------------------------------
+// std::transform
+//-----------------------------------------------------------------------------
+
+namespace transform_suite
+{
+
+auto identity = [] (const variable& value) -> variable { return value; };
+
+void transform_null()
+{
+    variable data;
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+}
+
+void transform_boolean()
+{
+    variable data(true);
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<boolean>());
+    TRIAL_PROTOCOL_TEST(data == true);
+}
+
+void transform_integer()
+{
+    variable data(2);
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<integer>());
+    TRIAL_PROTOCOL_TEST(data == 2);
+}
+
+void transform_number()
+{
+    variable data(3.0);
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<number>());
+    TRIAL_PROTOCOL_TEST(data == 3.0);
+}
+
+void transform_string()
+{
+    variable data("alpha");
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<string>());
+    TRIAL_PROTOCOL_TEST(data == "alpha");
+}
+
+void transform_wstring()
+{
+    variable data(L"bravo");
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<wstring>());
+    TRIAL_PROTOCOL_TEST(data == L"bravo");
+}
+
+void transform_u16string()
+{
+    variable data(u"charlie");
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<u16string>());
+    TRIAL_PROTOCOL_TEST(data == u"charlie");
+}
+
+void transform_u32string()
+{
+    variable data(U"delta");
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<u32string>());
+    TRIAL_PROTOCOL_TEST(data == U"delta");
+}
+
+void transform_array()
+{
+    variable data = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+    auto where = std::transform(data.begin(), data.end(), data.begin(), identity);
+    TRIAL_PROTOCOL_TEST(where == data.end());
+    TRIAL_PROTOCOL_TEST(data.is<array>());
+    variable expect = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+    TRIAL_PROTOCOL_TEST_ALL_WITH(data.begin(), data.end(),
+                                 expect.begin(), expect.end(),
+                                 std::equal_to<variable>());
+}
+
+void run()
+{
+    transform_null();
+    transform_boolean();
+    transform_integer();
+    transform_number();
+    transform_string();
+    transform_wstring();
+    transform_u16string();
+    transform_u32string();
+    transform_array();
+}
+
+} // namespace transform_suite
+
+//-----------------------------------------------------------------------------
 // std::unique
 //-----------------------------------------------------------------------------
 
@@ -7887,6 +7993,7 @@ int main()
     remove_suite::run();
     reverse_suite::run();
     search_suite::run();
+    transform_suite::run();
     unique_suite::run();
     upper_bound_suite::run();
 
