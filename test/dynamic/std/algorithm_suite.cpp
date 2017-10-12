c45b6ccabe6f7c0117d572d6cf944a58b8b6a719
Bjorn Reese <breese@users.sourceforge.net>
2017-10-12 18:44:04 +0200

    Added test suite for std::move_backward

--- test/dynamic/std/algorithm_suite.cpp
+++ test/dynamic/std/algorithm_suite.cpp
@@ -4909,6 +4909,111 @@ void run()
 } // namespace move_suite
 
 //-----------------------------------------------------------------------------
+// std::move_backward
+//-----------------------------------------------------------------------------
+
+namespace move_backward_suite
+{
+
+void move_null_to_array()
+{
+    variable data;
+    variable result = array::repeat(0, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 0);
+}
+
+void move_boolean_to_array()
+{
+    variable data(true);
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == true);
+}
+
+void move_integer_to_array()
+{
+    variable data(2);
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 2);
+}
+
+void move_number_to_array()
+{
+    variable data(3.0);
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == 3.0);
+}
+
+void move_string_to_array()
+{
+    variable data("alpha");
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == "alpha");
+}
+
+void move_wstring_to_array()
+{
+    variable data(L"bravo");
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == L"bravo");
+}
+
+void move_u16string_to_array()
+{
+    variable data(u"charlie");
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == u"charlie");
+}
+
+void move_u32string_to_array()
+{
+    variable data(U"delta");
+    variable result = array::repeat(1, null);
+    std::move_backward(data.begin(), data.end(), result.end());
+    TRIAL_PROTOCOL_TEST_EQUAL(result.size(), 1);
+    TRIAL_PROTOCOL_TEST(result[0] == U"delta");
+}
+
+void move_array_to_array()
+{
+    variable data = { true, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" };
+    variable result = array::repeat(data.size(), null);
+    std::move_backward(data.begin(), data.end(), result.end());
+
+    variable expect = array::make({ true, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+    TRIAL_PROTOCOL_TEST_ALL_WITH(result.begin(), result.end(),
+                                 expect.begin(), expect.end(),
+                                 std::equal_to<variable>());
+}
+
+void run()
+{
+    move_null_to_array();
+    move_boolean_to_array();
+    move_integer_to_array();
+    move_number_to_array();
+    move_string_to_array();
+    move_wstring_to_array();
+    move_u16string_to_array();
+    move_u32string_to_array();
+    move_array_to_array();
+}
+
+} // namespace move_backward_suite
+
+//-----------------------------------------------------------------------------
 // std::none_of
 //-----------------------------------------------------------------------------
 
@@ -7775,6 +7880,7 @@ int main()
     max_element_suite::run();
     mismatch_suite::run();
     move_suite::run();
+    move_backward_suite::run();
     none_of_suite::run();
     partition_suite::run();
     partition_point_suite::run();
