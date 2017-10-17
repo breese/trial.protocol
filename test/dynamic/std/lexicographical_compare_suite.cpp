5f6487ab74638d76aaaff842d2b0a7fd70d7fa1e
Bjorn Reese <breese@users.sourceforge.net>
2017-10-17 15:50:19 +0200

    Added test suite for std::lexicographical_compare

---
+++ test/dynamic/std/lexicographical_compare_suite.cpp
@@ -0,0 +1,409 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2017 Bjorn Reese <breese@users.sourceforge.net>
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <algorithm>
+#include <functional>
+#include <trial/protocol/core/detail/lightweight_test.hpp>
+#include <trial/dynamic/variable.hpp>
+
+using namespace trial::dynamic;
+
+//-----------------------------------------------------------------------------
+// std::lexicographical_compare
+//-----------------------------------------------------------------------------
+
+void compare_null()
+{
+    variable data;
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+
+    // null - boolean
+    {
+        variable other(true);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - integer
+    {
+        variable other(2);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - number
+    {
+        variable other(3.0);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - string
+    {
+        variable other("alpha");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - wstring
+    {
+        variable other(L"bravo");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - u16string
+    {
+        variable other(u"charlie");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // null - u32string
+    {
+        variable other(U"delta");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+}
+
+void compare_boolean()
+{
+    variable data(true);
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+    // boolean - null
+    {
+        // nullable is an empty container and an empty range is lexicographically
+        // less than a non-empty range.
+
+        variable other;
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // boolean - boolean
+    {
+        variable other(false);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // boolean - integer
+    {
+        variable other(2);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // boolean - number
+    {
+        variable other(3.0);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // boolean - string
+    {
+        variable other("alpha");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // boolean - wstring
+    {
+        variable other(L"bravo");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // boolean - u16string
+    {
+        variable other(u"charlie");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // boolean - u32string
+    {
+        variable other(U"delta");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+}
+
+void compare_integer()
+{
+    variable data(2);
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+    // integer - null
+    {
+        // See boolean - null comparison
+
+        variable other;
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // integer - boolean
+    {
+        variable other(true);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // integer - integer
+    {
+        variable smaller(0);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger(22);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                          larger.begin(), larger.end()));
+    }
+    // integer - number
+    {
+        variable smaller(0.0);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger(3.0);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // integer - string
+    {
+        variable other("alpha");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // integer - wstring
+    {
+        variable other(L"bravo");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // integer - u16string
+    {
+        variable other(u"charlie");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // integer - u32string
+    {
+        variable other(U"delta");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+}
+
+void compare_number()
+{
+    variable data(3.0);
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+    // number - null
+    {
+        // See boolean - null comparison
+
+        variable other;
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // number - boolean
+    {
+        variable other(true);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // number - integer
+    {
+        variable smaller(0);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger(22);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                          larger.begin(), larger.end()));
+    }
+    // number - number
+    {
+        variable smaller(0.0);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger(33.0);
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // number - string
+    {
+        variable other("alpha");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // number - wstring
+    {
+        variable other(L"bravo");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // number - u16string
+    {
+        variable other(u"charlie");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // number - u32string
+    {
+        variable other(U"delta");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+}
+
+void compare_string()
+{
+    variable data("alpha");
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+    // string - null
+    {
+        // See boolean - null comparison
+
+        variable other;
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // string - boolean
+    {
+        variable other(true);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // string - integer
+    {
+        variable other(2);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // string - number
+    {
+        variable other(3.0);
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          other.begin(), other.end()));
+    }
+    // string - string
+    {
+        variable other("zulu");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // string - wstring
+    {
+        variable other(L"bravo");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // string - u16string
+    {
+        variable other(u"charlie");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+    // string - u32string
+    {
+        variable other(U"delta");
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         other.begin(), other.end()));
+    }
+}
+
+void compare_array_array()
+{
+    variable data = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end()));
+    TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                      data.begin(), data.end(),
+                                                      std::less<variable>()));
+
+    // boolean
+    {
+        variable smaller = array::make({ null, false, 2, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+    }
+    // integer
+    {
+        variable smaller = array::make({ null, true, 0, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 22, 3.0, "alpha", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // number
+    {
+        variable smaller = array::make({ null, true, 2, 0.0, "alpha", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 2, 33.0, "alpha", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // string
+    {
+        variable smaller = array::make({ null, true, 2, 3.0, "", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 2, 3.0, "zulu", L"bravo", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // wstring
+    {
+        variable smaller = array::make({ null, true, 2, 3.0, "alpha", L"", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 2, 3.0, "alpha", L"zulu", u"charlie", U"delta" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // u16string
+    {
+        variable smaller = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"", U"delta" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"zulu", U"delta" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+    // u32string
+    {
+        variable smaller = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"charlie", U"" });
+        TRIAL_PROTOCOL_TEST(!std::lexicographical_compare(data.begin(), data.end(),
+                                                          smaller.begin(), smaller.end()));
+        variable larger = array::make({ null, true, 2, 3.0, "alpha", L"bravo", u"charlie", U"zulu" });
+        TRIAL_PROTOCOL_TEST(std::lexicographical_compare(data.begin(), data.end(),
+                                                         larger.begin(), larger.end()));
+    }
+}
+
+int main()
+{
+    compare_null();
+    compare_boolean();
+    compare_integer();
+    compare_number();
+    compare_string();
+    compare_array_array();
+
+    return boost::report_errors();
+}
