5af2ae76e14b87eaef8878902e0cc71c9be83daf
Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
2021-07-28 20:40:50 -0300

    Add json::partial::copy_and_look_ahead()
    Updates #26

---
+++ include/trial/protocol/json/partial/copy_and_look_ahead.hpp
@@ -0,0 +1,176 @@
+#ifndef TRIAL_PROTOCOL_JSON_PARTIAL_COPY_AND_LOOK_AHEAD_HPP
+#define TRIAL_PROTOCOL_JSON_PARTIAL_COPY_AND_LOOK_AHEAD_HPP
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// Copyright (C) 2021 Vinícius dos Santos Oliveira <vini.ipsmaker@gmail.com>
+//
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#include <trial/protocol/json/partial/skip.hpp>
+
+namespace trial
+{
+namespace protocol
+{
+namespace json
+{
+namespace partial
+{
+
+namespace detail {
+template<class CharT>
+struct CopyAndLookAheadCollector
+{
+    using view_type = typename basic_reader<CharT>::view_type;
+    using size_type = typename basic_reader<CharT>::size_type;
+
+    CopyAndLookAheadCollector(view_type key)
+        : pattern{key}
+    {}
+
+    void push_back(CharT v)
+    {
+        if (!process) return;
+
+        if (!pattern.starts_with(v)) {
+            process = false;
+            return;
+        }
+
+        pattern.remove_prefix(1);
+    }
+
+    void append(const CharT *data, size_type size)
+    {
+        if (!process) return;
+
+        view_type run{data, size};
+        if (!pattern.starts_with(run)) {
+            process = false;
+            return;
+        }
+
+        pattern.remove_prefix(size);
+    }
+
+    bool current_value_matches_pattern()
+    {
+        return process && pattern.size() == 0;
+    }
+
+    bool process = true;
+    view_type pattern;
+};
+} // namespace detail
+
+template<class CharT, class F>
+auto/*=bool*/ copy_and_look_ahead(const basic_reader<CharT> &reader_,
+                                  typename basic_reader<CharT>::view_type key,
+                                  F&& on_value, std::error_code &ec)
+    -> decltype(on_value(const_cast<basic_reader<CharT>&>(reader_)), true)
+{
+    assert(!ec);
+
+    if (reader_.symbol() != json::token::symbol::begin_object) {
+        ec = errc::field_not_found;
+        return false;
+    }
+
+    basic_reader<CharT> reader(reader_);
+
+    if (!reader.next()) {
+        ec = reader.error();
+        return false;
+    }
+
+    for (;;) {
+        if (reader.symbol() == json::token::symbol::end_object) {
+            ec = errc::field_not_found;
+            return false;
+        }
+
+        // Key
+        assert(reader.symbol() == json::token::symbol::key);
+        detail::CopyAndLookAheadCollector<CharT> collector{/*pattern=*/key};
+        {
+            auto ec = reader.string(collector);
+            assert(!ec); (void)ec;
+        }
+        if (!reader.next()) {
+            ec = reader.error();
+            return false;
+        }
+
+        // Value
+        if (!collector.current_value_matches_pattern()) {
+            skip(reader, ec);
+            if (ec)
+                return false;
+            continue;
+        }
+
+        on_value(reader);
+        return true;
+    }
+}
+
+template<class CharT, class F>
+auto/*=bool*/ copy_and_look_ahead(const basic_reader<CharT> &reader,
+                                  typename basic_reader<CharT>::view_type key,
+                                  F&& on_value)
+    -> decltype(on_value(const_cast<basic_reader<CharT>&>(reader)), true)
+{
+    std::error_code ec;
+    bool ret = copy_and_look_ahead(reader, key, std::forward<F>(on_value), ec);
+    if (ec)
+        throw json::error(ec);
+    return ret;
+}
+
+template<class CharT>
+bool copy_and_look_ahead(const basic_reader<CharT> &reader,
+                         const typename basic_reader<CharT>::view_type key,
+                         const typename basic_reader<CharT>::view_type value,
+                         std::error_code &ec)
+{
+    bool ret = false;
+    auto on_value = [&value,&ec,&ret](basic_reader<CharT> &reader) {
+        if (reader.symbol() != json::token::symbol::string)
+            return;
+
+        detail::CopyAndLookAheadCollector<CharT> collector{/*pattern=*/value};
+        {
+            auto ec = reader.string(collector);
+            assert(!ec); (void)ec;
+        }
+        ret = collector.current_value_matches_pattern();
+        if (!ret)
+            ec = errc::field_not_found;
+    };
+    copy_and_look_ahead(reader, key, on_value, ec);
+    return ret;
+}
+
+template<class CharT>
+bool copy_and_look_ahead(const basic_reader<CharT> &reader,
+                         const typename basic_reader<CharT>::view_type key,
+                         const typename basic_reader<CharT>::view_type value)
+{
+    std::error_code ec;
+    bool ret = copy_and_look_ahead(reader, key, value, ec);
+    if (ec)
+        throw json::error(ec);
+    return ret;
+}
+
+} // namespace partial
+} // namespace json
+} // namespace protocol
+} // namespace trial
+
+#endif // TRIAL_PROTOCOL_JSON_PARTIAL_COPY_AND_LOOK_AHEAD_HPP
